5969979aad1c4f9c84269c4b7cdf84bbd0fbccda
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-02-19 13:07:59 +0200

    Update ziad.py

--- ziad.py
+++ ziad.py
@@ -14,7 +14,7 @@ from streamlit_lottie import st_lottie
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
+from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(
     page_title="Beirut Port Explosion",
