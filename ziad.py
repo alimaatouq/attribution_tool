7c1ba447a55dbfa9db82c00b3f618a996c402af9
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-02-19 12:30:00 +0200

    Update ziad.py

--- ziad.py
+++ ziad.py
@@ -23,6 +23,15 @@ st.set_page_config(
 )
 
 
+# Load css style file from local disk
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+# Load css style from url
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">',unsafe_allow_html = True)
+
+
 menu_data = [
     {'label': "Overview", 'icon': 'bi bi-bar-chart-line'},
     {'label': 'Tableau', 'icon': 'bi bi-clipboard-data'},
