ce8013675efd7b195536209b4d77ec8f98e1e04e
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-02-26 12:02:51 +0200

    Update ziad.py

--- ziad.py
+++ ziad.py
@@ -18,7 +18,7 @@ from plotly.subplots import make_subplots
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.linear_model import LogisticRegression
 
 st.set_page_config(
     page_title="Beirut Port Explosion",
@@ -314,7 +314,7 @@ if menu_id == "Application":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Initialize and train a Random Forest Classifier (you can choose a different classifier)
-    classifier = RandomForestClassifier(random_state=42)
+    classifier = LogisticRegression()
     classifier.fit(X_train, y_train)
 
     def user_report():
