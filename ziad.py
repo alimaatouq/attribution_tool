261ae36f5458553b94e24aef858f8523bdef26dd
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-02-26 12:25:12 +0200

    Update ziad.py

--- ziad.py
+++ ziad.py
@@ -18,7 +18,6 @@ from plotly.subplots import make_subplots
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 
 st.set_page_config(
     page_title="Beirut Port Explosion",
