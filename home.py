02050ce8b810117a4ef7a0b9cd100e8b8b81c2be
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-10-28 18:00:29 +0300

    Update home.py

--- home.py
+++ home.py
@@ -5,12 +5,15 @@ st.set_page_config(page_title="Multipage App", layout='wide')
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Path Editor", "Paid Media Spends", "Submodel Analysis"])
+page = st.sidebar.radio(
+    "Go to", 
+    ["Path Editor", "Paid Media Spends", "Submodel Analysis"]
+)
 
 # Load and display the appropriate page
 if page == "Path Editor":
-    exec(Path("pages/1_Path_Editor.py").read_text())
+    exec(Path("pages/Path_Editor.py").read_text(), globals())
 elif page == "Paid Media Spends":
-    exec(Path("pages/2_Paid_Media_Spends_Vars.py").read_text())
+    exec(Path("pages/Paid_Media_Spends_Vars.py").read_text(), globals())
 elif page == "Submodel Analysis":
-    exec(Path("pages/3_Sub_Model_Analysis.py").read_text())
+    exec(Path("pages/Sub_Model_Analysis.py").read_text(), globals())
