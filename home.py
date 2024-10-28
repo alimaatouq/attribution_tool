9885bf5e9884918a5a5b1dadc8af39e943cf4481
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-10-28 18:16:50 +0300

    Update home.py

--- home.py
+++ home.py
@@ -2,11 +2,3 @@ import streamlit as st
 from pathlib import Path
 
 st.set_page_config(page_title="Attribution Multipage App", layout='wide')
-
-# Load and display the appropriate page
-if page == "Path Editor":
-    exec(Path("pages/Path_Editor.py").read_text())
-elif page == "Paid Media Spends":
-    exec(Path("pages/Paid_Media_Spends_Vars.py").read_text())
-elif page == "Submodel Analysis":
-    exec(Path("pages/Sub_Model_Analysis.py").read_text())
