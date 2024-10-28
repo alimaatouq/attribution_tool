758832ffce7b19177634c54e3cf5f482bd7b8e3a
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-10-28 17:56:16 +0300

    Update home.py

--- home.py
+++ home.py
@@ -9,8 +9,8 @@ page = st.sidebar.radio("Go to", ["Path Editor", "Paid Media Spends", "Submodel 
 
 # Load and display the appropriate page
 if page == "Path Editor":
-    exec(Path("pages/Path_Editor.py").read_text())
+    exec(Path("pages/1_Path_Editor.py").read_text())
 elif page == "Paid Media Spends":
-    exec(Path("pages/Paid_Media_Spends_Vars.py").read_text())
+    exec(Path("pages/2_Paid_Media_Spends_Vars.py").read_text())
 elif page == "Submodel Analysis":
-    exec(Path("pages/Sub_Model_Analysis.py").read_text())
+    exec(Path("pages/3_Sub_Model_Analysis.py").read_text())
