1d62eda3d995485557c4c8ba3a1328cad5794a93
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-10-28 18:28:19 +0300

    Update home.py

--- home.py
+++ home.py
@@ -8,13 +8,14 @@ st.write("# Welcome to the Data Attribution Tool! 👋")
 
 st.markdown("""
 An all-in-one platform designed to streamline your data handling tasks. This app offers powerful tools to assist with path editing, media spend analysis, and submodel evaluations. Navigate through the pages using the sidebar to access the following features:
-📂 Path Editor
+
+\n📂 Path Editor
 
 Easily reformat file paths by converting slashes to double backslashes for use in various environments. Perfect for Windows directory paths or coding scenarios requiring special formatting.
-📊 Paid Media Spends Analyzer
+\n📊 Paid Media Spends Analyzer
 
 Upload your Excel files to extract columns related to media spends and impressions. Quickly copy pre-formatted outputs for further use in R or other statistical tools.
-📈 Submodel Analysis Tool
+\n 📈 Submodel Analysis Tool
 
 Analyze models from Excel or CSV files to identify submodels with non-zero coefficients and generate insightful summaries. Understand where zero-coefficient variables impact your spend and optimize your strategies effectively.
 """)
