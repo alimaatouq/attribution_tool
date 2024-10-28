cef1a1cdb9340583eddd66c3fa6501aee6edfd3d
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-10-28 17:54:59 +0300

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,16 @@
 import streamlit as st
+from pathlib import Path
 
-st.set_page_config(
-    page_title="Attribution tool",
-    page_icon="👋",
-)
+st.set_page_config(page_title="Multipage App", layout='wide')
 
-st.title("Attribution Tool Main Page")
-st.sidebar.success("Select a page above.")
+# Sidebar for navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Path Editor", "Paid Media Spends", "Submodel Analysis"])
+
+# Load and display the appropriate page
+if page == "Path Editor":
+    exec(Path("pages/Path_Editor.py").read_text())
+elif page == "Paid Media Spends":
+    exec(Path("pages/Paid_Media_Spends_Vars.py").read_text())
+elif page == "Submodel Analysis":
+    exec(Path("pages/Sub_Model_Analysis.py").read_text())
