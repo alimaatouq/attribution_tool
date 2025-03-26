c024fce13972ed2c86d6daf2a3dc3b4ac7fd8b0d
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2025-03-26 22:23:33 +0200

    Update home.py

--- home.py
+++ home.py
@@ -36,3 +36,15 @@ page_style= """
     </style>"""
 
 st.markdown(page_style, unsafe_allow_html=True)
+
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+)
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
