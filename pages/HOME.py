bbd55d7bdefdb5dc38b1bd56c822b5a925645f3f
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2025-03-26 22:40:38 +0200

    Update HOME.py

--- pages/HOME.py
+++ pages/HOME.py
@@ -2,9 +2,6 @@ import streamlit as st
 from pathlib import Path
 from st_pages import add_page_title, get_nav_from_toml
 
-
-st.set_page_config(page_title="Attribution Multipage App", layout='wide')
-
 st.write("# Welcome to the Attribution Tool! 👋")
 
 st.markdown("""
