2ed31f744b2dcdd8f03f1ae968afc4df9788de63
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2025-03-26 20:45:29 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pathlib import Path
+from st_pages import add_page_title, get_nav_from_toml
+
 
 st.set_page_config(page_title="Attribution Multipage App", layout='wide')
 st.sidebar.success("Select a demo above.")
